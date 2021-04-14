bbb74d2f3c447414f4c13aa8a90cf81f90ee6f7a
miktim <miktim@mail.ru>
2021-04-14 10:24:48 +0300

    0.0.1

--- src/org/miktim/udpserver/UdpServer.java
+++ src/org/miktim/udpserver/UdpServer.java
@@ -1,6 +1,6 @@
 /*
  * UdpServer, MIT (c) 2019-2021 miktim@mail.ru
- * UDP unicast/multicast sender/receiver
+ * UDP broadcast/unicast/multicast sender/receiver
  */
 package org.miktim.udpserver;
 
@@ -8,110 +8,59 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
-import java.util.ArrayDeque;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
 
 public class UdpServer extends Thread {
 
     public interface Handler {
-
+        void onOpen(UdpServer server);
         void onUdpPacket(UdpServer server, DatagramPacket packet);
-
         void onUdpError(UdpServer server, Exception e);
+        void onClose(UdpServer server);
     }
 
-    private DatagramSocket ds;
-    private MulticastSocket ms = null;
-    private int bufferLength = 256;
+    private DatagramSocket socket;
+    private int port;               // bind port (local & remote)
+    private InetAddress inetAddress; // connect address
+    private InetAddress bindAddress; // bind (interface) address
+    private int bufferLength = 508; // 508 - IPv4 guaranteed receive packet size by any host
+    // SO_RCVBUF size = 106496 (Linux x64)
     private Handler handler;
 
-    private final ArrayDeque<DatagramPacket> packetQueue = new ArrayDeque();
+//    private final ArrayDeque<DatagramPacket> packetQueue = new ArrayDeque();
 
-    private class PacketDequer extends Thread {
+// port - binding (listening), connection, group port
+// inetAddr - connection/multicast group address
+// bindAddr - socket binding (interface) address
+    public UdpServer(int port, UdpServer.Handler handler) throws Exception {
+        createServer(port, null, null, handler);
+    }
 
-        UdpServer server;
-        DatagramPacket dp;
+    public UdpServer(int port, InetAddress inetAddress, UdpServer.Handler handler)
+            throws Exception {
+        createServer(port, inetAddress, null, handler);
+    }
 
-        PacketDequer(UdpServer server) {
-            this.server = server;
+    public UdpServer(int port, InetAddress inetAddress, InetAddress bindAddr, UdpServer.Handler handler)
+            throws Exception {
+        createServer(port, inetAddress, bindAddr, handler);
+    }
+
+    public boolean isMulticastSocket() {
+        return inetAddress.isMulticastAddress();
+    }
+
+    public DatagramSocket getSocket() {
+        return socket;
+    }
+
+    public void setBufferLength(int length) throws IllegalArgumentException {
+        if (length <= 0) {
+            throw new IllegalArgumentException();
         }
-
-        @Override
-        public void run() {
-
-            while (!server.ds.isClosed()) {
-                synchronized (server.packetQueue) {
-                    if (!server.packetQueue.isEmpty()) {
-                        dp = server.packetQueue.pollFirst();
-                        server.handler.onUdpPacket(server, dp);
-                    } else {
-                        try {
-                            server.packetQueue.wait();
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public UdpServer(int port, Handler handler) throws IOException {
-        create(port, null, handler);
-    }
-
-    public UdpServer(int port, InetAddress inetAddress, Handler handler) throws IOException {
-        create(port, inetAddress, handler);
-    }
-
-    private void create(int port, InetAddress inetAddress, Handler handler) throws IOException {
-        InetAddress ia = inetAddress != null ? inetAddress : InetAddress.getByName("0.0.0.0");
-        if (ia.isMulticastAddress()) {
-            ms = new MulticastSocket(port);
-            ms.joinGroup(ia);
-            ms.setLoopbackMode(false);
-            if (ia.isMCLinkLocal()) {
-                ms.setTimeToLive(1);
-            }
-            ds = ms;
-        } else {
-            ds = new DatagramSocket(port, ia);
-            if (ds.getInetAddress().isAnyLocalAddress()) {
-                ds.setBroadcast(true);
-            }
-        }
-        this.handler = handler;
-    }
-
-    public boolean isMulticast() {
-        return ms != null;
-    }
-    
-//    public MulticastSocket getMulticastSocket() {
-//        return ms;
-//    }
-
-    public DatagramSocket getDatagramSocket() {
-        return ds;
-    }
-
-    public void send(byte[] packet) throws IOException {
-            DatagramPacket dp = new DatagramPacket(packet, packet.length);
-            ds.send(dp);
-    }
-
-    /*
-    public void send(byte[] packet, InetAddress inetAddress) {
-        try {
-            DatagramPacket dp = new DatagramPacket(packet, packet.length);
-            dp.setAddress(inetAddress);
-            ds.send(dp);
-        } catch (IOException e) {
-            listener.onUdpError(e);
-        }
-    }
-     */
-    public void setBufferLength(int length) {
         bufferLength = length;
     }
 
@@ -119,39 +68,118 @@ public class UdpServer extends Thread {
         return bufferLength;
     }
 
+    final void createServer(int port, InetAddress inetAddr, InetAddress bindAddr, UdpServer.Handler handler) throws UnknownHostException, IOException {
+
+        this.port = port;
+        this.inetAddress = inetAddr != null ? inetAddr : InetAddress.getByName("0.0.0.0");
+        this.bindAddress = bindAddr;
+        this.handler = handler;
+
+        if (inetAddress.isMulticastAddress()) {
+// See note:
+//   https://docs.oracle.com/javase/7/docs/api/java/net/DatagramSocket.html#setReuseAddress(boolean)
+            if (bindAddress != null) {
+                socket = new MulticastSocket();
+                ((MulticastSocket) socket).joinGroup(
+                        new InetSocketAddress(inetAddr, port),
+                        NetworkInterface.getByInetAddress(bindAddr)
+                );
+            } else {
+                socket = new MulticastSocket(port);
+                ((MulticastSocket) socket).joinGroup(inetAddress);
+            }
+            socket.setReuseAddress(true);
+        } else {
+            if (bindAddress != null) {
+                socket = new DatagramSocket(port, bindAddress);
+            } else {
+                socket = new DatagramSocket(port);
+            }
+            if (inetAddr != null) { // if null listen only
+                if (inetAddress.isAnyLocalAddress()) {
+                    socket.setBroadcast(true);
+                } else {
+                    socket.connect(inetAddress, port);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        if (handler == null) {
+            throw new NullPointerException("No handler");
+        }
+        super.start();
+    }
+
     @Override
     public void run() {
-        new PacketDequer(this).start();
-        while (!ds.isClosed()) {
+        handler.onOpen(this);
+        while (!socket.isClosed()) {
             try {
                 DatagramPacket dp
                         = new DatagramPacket(new byte[bufferLength], bufferLength);
-                ds.receive(dp);
-                synchronized (packetQueue) {
-                    packetQueue.addLast(dp);
-                    packetQueue.notify();
-                }
+                socket.receive(dp);
+            } catch (java.net.SocketTimeoutException e) {
+// ignore
             } catch (IOException e) {
+                if (socket.isClosed()) {
+                    break;
+                }
                 handler.onUdpError(this, e);
             }
         }
+        handler.onClose(this);
     }
-    public boolean isListening() {
-        Thread.State state = this.getState();
-        return !(state == State.NEW || state == State.TERMINATED);
+
+    @Override
+    public String toString() {
+        String serverType = inetAddress.isAnyLocalAddress() ? "Broadcast "
+                : (inetAddress.isMulticastAddress() ? "Multicast "
+                : "Unicast ") + "UDP socket";
+        String mcGroup = inetAddress.isMulticastAddress()
+                ? " MCgroup ("
+                + (inetAddress.isMCGlobal() ? "global," : "local,")
+                + inetAddress
+                + bindAddress != null ? "," + port : ""
+                        + ")"
+                : "";
+        String boundTo = " is bound to ("
+                + socket.getLocalSocketAddress()
+                + ","
+                + socket.getLocalPort()
+                + ")";
+        String connectedTo = socket.isConnected()
+                ? " connected to " + inetAddress
+                : "";
+        return serverType + mcGroup + boundTo + connectedTo;
     }
-    
+
     public void close() {
-        if (ds != null) {
-            ds.close();
-        }
-        try {
-            if (ms != null) {
-                ms.leaveGroup(ms.getInetAddress());
+        if (!socket.isClosed()) {
+            try { // ???
+                if (isMulticastSocket()) {
+                    if (bindAddress != null) {
+                        ((MulticastSocket) socket).leaveGroup(
+                                new InetSocketAddress(inetAddress, port),
+                                NetworkInterface.getByInetAddress(bindAddress)
+                        );
+                    } else {
+                        ((MulticastSocket) socket).leaveGroup(inetAddress);
+                    }
+                }
+                if (socket.isConnected()) {
+                    socket.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            socket.close();
         }
-        packetQueue.clear();
+    }
+
+    public void send(byte[] datagram) throws IOException {
+        socket.send(new DatagramPacket(datagram, datagram.length, inetAddress, port));
     }
 }
---
+++ test/udpserver/UdpServerTest.java
@@ -0,0 +1,66 @@
+/*
+ * UdpServerTest, MIT (c) 2021 miktim@mail.ru
+ */
+package udpserver;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.miktim.udpserver.UdpServer;
+
+public class UdpServerTest {
+
+    static final int UDP_PORT = 9090;
+    static final String UDP_ADDRESS = "localhost";
+    static final int SERVER_SHUTDOWN_TIMEOUT = 2000;
+
+    public static void log(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        UdpServer.Handler handler = new UdpServer.Handler() {
+            @Override
+            public void onOpen(UdpServer server) {
+                log("Server started. " + server.toString());
+            }
+
+            @Override
+            public void onClose(UdpServer server) {
+                log("Server stopped.");
+            }
+
+            @Override
+            public void onUdpPacket(UdpServer server, DatagramPacket packet) {
+                log("Receiver onPacket: size " + packet.getLength());
+            }
+
+            @Override
+            public void onUdpError(UdpServer server, Exception e) {
+                log("Receiver onError: " + e);
+            }
+
+        };
+        UdpServer sender = new UdpServer(UDP_PORT, InetAddress.getByName(UDP_ADDRESS), null);
+        final UdpServer server = new UdpServer(UDP_PORT, InetAddress.getByName(UDP_ADDRESS), handler);
+        server.start();
+        sender.send(new byte[server.getBufferLength()]);
+        sender.send(new byte[server.getBufferLength() * 2]);
+        Thread.sleep(50);
+        server.setBufferLength(server.getBufferLength() * 2);
+        sender.send(new byte[server.getBufferLength()]);
+        sender.send(new byte[server.getBufferLength() * 2]);
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                server.close();
+                timer.cancel();
+            }
+        }, SERVER_SHUTDOWN_TIMEOUT);
+
+    }
+}
