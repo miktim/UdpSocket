4145974a52a0182ca6d83967c2cc9be850828361
miktim <miktim@mail.ru>
2025-02-02 08:47:03 +0300

    3.1.2

--- src/org/miktim/udpsocket/UdpSocket.java
+++ src/org/miktim/udpsocket/UdpSocket.java
@@ -16,7 +16,7 @@ import java.net.SocketException;
 
 public final class UdpSocket extends Thread implements Closeable, AutoCloseable {
 
-    public static final String VERSION = "3.1.1";
+    public static final String VERSION = "3.1.2";
 
     public interface Handler {
 
